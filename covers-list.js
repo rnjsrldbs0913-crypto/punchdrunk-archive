dc2544b4d400a28f319c1a6ce398bb382314b4ad
rnjsrldbs0913-crypto <rnjsrldbs0913@gmail.com>
2026-09-15 14:07:06 +0900

    rr

--- covers-list.js
+++ covers-list.js
@@ -408,7 +408,15 @@ const COVER_IMAGES = [
   "covers/album-mu14v0w2.jpg",
   "covers/album-mu14rf1n.jpg",
   "covers/album-mu14npzh.jpg",
-  "covers/album-mu14ld4z.jpg"
+  "covers/album-mu14ld4z.jpg",
+  "covers/comfort-zone.jpg",
+  "covers/images-11.jpg",
+  "covers/images-12.jpg",
+  "covers/ab67616d0000b273328f39a660c26d706e7f069b-1.jpg",
+  "covers/ab67616d0000b273016203ebe0693797cecd9c37-1.jpg",
+  "covers/ab67616d0000b27330a6c9a5e5992b76a02db960-1.jpg",
+  "covers/album-mu27jq08.jpg",
+  "covers/album-mu27hb75.jpg"
 ];
 
 window.PD_COVERS = COVER_IMAGES;
